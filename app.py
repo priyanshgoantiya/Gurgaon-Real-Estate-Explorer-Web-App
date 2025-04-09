18fd047e617252bd3b3c6b512782a3bfbf779330
Priyansh Goantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-04-09 19:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ elif selection == "Insight Model":
     # Compute performance metrics
     from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
     mae = mean_absolute_error(y_true, y_pred)
-    rmse = mean_squared_error(y_true, y_pred, squared=False)
+    rmse = mean_squared_error(y_true, y_pred)
     r2 = r2_score(y_true, y_pred)
 
     # Display metrics
